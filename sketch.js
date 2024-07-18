8067041f5f71a20d22dcbe5aa31d6e64b4243a26
NehaBIngale <neha.ingale.sod23@anu.edu.in>
2024-07-18 16:03:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(700,480);
+  
+  // R,G,B values
+  background (130, 210, 245);
+  
+  //take a big brush
+  strokeWeight (20);
+}
+
+function draw() {
+  
+  let posX=mouseX
+  let posY=mouseY
+  point(posX,posY,)
+  fill(posX,20,30,20,28)
+  stroke(10,12,50,20,60)
+  
+  rect(mouseX,mouseY,55,80,20)
+  fill(mouseX,mouseY,30,80,20)
+  
+
+  
+  
+
+}
