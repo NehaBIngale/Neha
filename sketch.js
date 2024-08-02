f8659f68243a514626178920c594d2e50ba58f1c
NehaBIngale <neha.ingale.sod23@anu.edu.in>
2024-08-02 15:05:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,26 @@
-// Ball properties
-let ball = {
-  x: 100,       // X position
-  y: 100,       // Y position
-  diameter: 50,  // Diameter
-  xSpeed: 5,     // X velocity
-  ySpeed: 3      // Y velocity
-};
-
 function setup() {
-  createCanvas(600, 400);  // Create a canvas with width 600 and height 400
-  noStroke();             // Disable the outline of shapes
+  createCanvas(700,480);
+  
+  // R,G,B values
+  background (130, 210, 245);
+  
+  //take a big brush
+  strokeWeight (20);
 }
 
 function draw() {
-  background(220);        // Clear the screen with a light gray background
   
-  // Update ball position
-  ball.x += ball.xSpeed;
-  ball.y += ball.ySpeed;
+  let posX=mouseX
+  let posY=mouseY
+  point(posX,posY,)
+  fill(posX,20,30,20,28)
+  stroke(10,12,50,20,60)
   
-  // Draw the ball
-  fill(255, 0, 0);        // Set the ball color to red
-  ellipse(ball.x, ball.y, ball.diameter, ball.diameter);
+  rect(mouseX,mouseY,55,80,20)
+  fill(mouseX,mouseY,30,80,20)
+  
 
-  // Check for collision with the left or right edge
-  if (ball.x < ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
-    ball.xSpeed *= -1;   // Reverse the horizontal direction
-  }
+  
+  
 
-  // Check for collision with the top or bottom edge
-  if (ball.y < ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
-    ball.ySpeed *= -1;   // Reverse the vertical direction
-  }
 }
