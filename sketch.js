b2e284db50f07ece08cafd51d465c06ba9ec762e
NehaBIngale <neha.ingale.sod23@anu.edu.in>
2024-08-02 23:51:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-  createCanvas(480, 500);
-  background(320);
+  createCanvas(400, 500);
+  background(320,250,180);
 }
 
 function draw() {
   // Check if the mouse is pressed
   if (mouseIsPressed) {
     // Draw a circle at the mouse position
-    fill("blue"); // Blue color
+    fill("blue"); // blue color
     noStroke(); // No outline
-    ellipse(mouseX, mouseY, 20, 20); // Circle with diameter 10
+    ellipse(mouseX, mouseY, 20, 20); // Circle with diameter 20
   }
 }
